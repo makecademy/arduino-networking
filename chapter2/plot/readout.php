013cc7bb90f9231e4c78c6b0bf7fe0476f3bbae6
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-07-09 18:37:10 +0200

    added comments chapter2

--- chapter2/plot/readout.php
+++ chapter2/plot/readout.php
@@ -1,17 +1,25 @@
 <?php 
 
+// Show errors
 error_reporting(E_ALL); 
 ini_set("display_errors", 1); 
 
+// Open database
 $db = new SQLite3('database.db');
 
+// Set default timezone
+date_default_timezone_set('America/Los_Angeles');
+
+// Get data
 $results = $db->query('SELECT id, timestamp, temperature, humidity FROM measurements');
 
+// Parse data
 while($row = $results->fetchArray())
 {
     $dataset[] = array(strtotime($row['timestamp']) * 1000,$row['temperature']);
 }
 
+// Return data
 echo json_encode($dataset);
 
 ?>
